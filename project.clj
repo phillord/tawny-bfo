4d124eb95fb5cdaf082a08d96bc5a327f2b823db
Phillip Lord <phillip.lord@russet.org.uk>
2020-02-11 15:24:07 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tawny-bfo "0.1.0-SNAPSHOT"
+(defproject tawny-bfo "0.1.0"
   :description "A Tawny-OWL import of the BFO Ontologies"
   :url "http://github.com/phillord/tawny-bfo"
   :license "https://opensource.org/licenses/BSD-3-Clause"
